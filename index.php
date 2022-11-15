1d8d6f00f62dda209ca9292522852bd542bdf0c2
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-11-15 06:41:22 -0800

    Crud form

--- index.php
+++ index.php
@@ -10,31 +10,20 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Bisimwa</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+                <a class="nav-link" aria-current="page" href="students.php">Eleves</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+                <a class="nav-link" aria-current="page" href="parametre.php">Parametres</a>
               </li>
             </ul>
             <form class="d-flex" role="search">
@@ -45,14 +34,8 @@
         </div>
       </nav>
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h3>Dashboard</h3>
-            </div>
-            <div class="col-md-6">
-                <h3>Resumer</h3>
-            </div>
-        </div>
+            <?php include('./views/registration.php');?>
+            <?php include('./views/datas.php');?>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
---
+++ views/datas.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card card-body shadow-sm">
+                <h3 class="alert alert-warning">Data...</h3>
+            </div>
+        </div>
+    </div>
+</div>
